e7a29c53eba71547cdf9443423a49b8a8e08b15a
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-08-02 02:43:53 +0530

    Create Ac .java

---
+++ DESIGN_PATTERN/COMMAND_DESIGN_PATTERN/Ac .java
@@ -0,0 +1,12 @@
+package DESIGN_PATTERN.COMMAND_DESIGN_PATTERN;
+
+public class Ac {
+
+    public void On(){
+        System.out.println("Ac is On: ");
+    }
+
+    public void Off(){
+        System.out.println("Ac is Off: ");
+    }
+}
