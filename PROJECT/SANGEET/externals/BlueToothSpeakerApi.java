670823ec5968b1b5d40d1a074c195adad0e78016
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-08-31 19:47:52 +0530

    Create BlueToothSpeakerApi.java

---
+++ PROJECT/SANGEET/externals/BlueToothSpeakerApi.java
@@ -0,0 +1,9 @@
+package DESIGN_PATTERN.SANGEET.externals;
+
+public class BlueToothSpeakerApi {
+
+    public void playSongViaBT(String song){
+        System.out.println("Play Song: "  + song);
+    }
+
+}
