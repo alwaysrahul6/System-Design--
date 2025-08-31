f9796b18f26fb674b1acce72cedba1afe6d818f2
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-08-31 19:48:12 +0530

    Add files via upload

---
+++ PROJECT/SANGEET/externals/HeadPhoneApi.java
@@ -0,0 +1,9 @@
+package DESIGN_PATTERN.SANGEET.externals;
+
+public class HeadPhoneApi {
+
+    public void playViaHP(String song){
+        System.out.println("Playing Song " + song);
+    }
+
+}
---
+++ PROJECT/SANGEET/externals/WiredSpeakerApi.java
@@ -0,0 +1,8 @@
+package DESIGN_PATTERN.SANGEET.externals;
+
+public class WiredSpeakerApi {
+
+    public void playSoundViaWS(String song){
+        System.out.println("Play Sound Through" + song);
+    }
+}
