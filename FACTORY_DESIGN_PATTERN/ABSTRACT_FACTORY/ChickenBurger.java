3ff2684528a9b3aff4a793ee6806b4c72b56053a
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-12-10 23:19:41 +0530

    Update ChickenBurger.java

--- FACTORY_DESIGN_PATTERN/ABSTRACT_FACTORY/ChickenBurger.java
+++ FACTORY_DESIGN_PATTERN/ABSTRACT_FACTORY/ChickenBurger.java
@@ -1,8 +1,9 @@
 package DESIGN_PATTERN.FACTORY_DESIGN_PATTERN.ABSTRACT_FACTORY;
-
+  
 public class ChickenBurger implements Burger{
     @Override
     public void prepareBurger() {
         System.out.println("Chicken Burger: ");
     }
 }
+
