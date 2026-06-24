e9b4b915d27844c7d0552ad6a39ed752e8ec3827
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2026-06-24 21:52:48 +0530

    Update RecipeFactory.java

--- FACTORY_DESIGN_PATTERN/ABSTRACT_FACTORY/RecipeFactory.java
+++ FACTORY_DESIGN_PATTERN/ABSTRACT_FACTORY/RecipeFactory.java
@@ -3,4 +3,4 @@ package DESIGN_PATTERN.FACTORY_DESIGN_PATTERN.ABSTRACT_FACTORY;
 public interface RecipeFactory {
     Burger createBurger(String type);
     ColdDrink createdrink(String type);
-}
+}. 
